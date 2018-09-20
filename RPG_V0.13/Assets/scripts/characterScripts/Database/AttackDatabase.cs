446ac2171ad5ce8838b2fef4efb9e8d4badbc0a7
chrisjusto <31364845+chrisjusto@users.noreply.github.com>
2018-09-20 16:04:38 -0700

    updatePreClass

--- RPG_V0.13/Assets/scripts/characterScripts/Database/AttackDatabase.cs
+++ RPG_V0.13/Assets/scripts/characterScripts/Database/AttackDatabase.cs
@@ -88,7 +88,7 @@ public class AttackDatabase : MonoBehaviour {
         AttackType[8] = "dark";
         AttackPowerAcc[8] = "x/100";
         AttackUses[8] = 10;
-        AttackDesc[8] = "taunts the target forcing them to only use damaging moves";
+        AttackDesc[8] = "ATTACK NOT AVAILABLE";
         AttackTypeValue[8] = 10;
 
         AttackName[9] = "psychic shot";
@@ -96,48 +96,56 @@ public class AttackDatabase : MonoBehaviour {
         AttackPowerAcc[9] = "120/100";
         AttackUses[9] = 5;
         AttackDesc[9] = "the user stabalizes there aim taking one turn to charge launching a powerful shot";
+        AttackTypeValue[9] = 11;
 
         AttackName[10] = "Poison Arrow";
         AttackType[10] = "Nature";
         AttackPowerAcc[10] = "30/100";
         AttackUses[10] = 20;
         AttackDesc[10] = "shoot a poison tipped arrow with a 25% chance to poinson the target";
+        AttackTypeValue[10] = 4;
 
         AttackName[11] = " frozen bear trap";
         AttackType[11] = "ice";
         AttackPowerAcc[11] = "x/50";
         AttackUses[11] = 5;
         AttackDesc[11] = "freeze target";
+        AttackTypeValue[11] = 7;
 
         AttackName[12] = "elvish song";
         AttackType[12] = "light";
         AttackPowerAcc[12] = "x/100";
         AttackUses[12] = 10;
         AttackDesc[12] = "call upon the old elvish divine to heal the user";
+        AttackTypeValue[12] = 9;
 
         AttackName[13] = "thunder storm";
         AttackType[13] = "electric";
         AttackPowerAcc[13] = "40/100";
         AttackUses[13] = 10;
         AttackDesc[13] = "a storm of lightning that always hits his mark, all of the active players";
+        AttackTypeValue[13] = 8;
 
         AttackName[14] = "meditate";
         AttackType[14] = "psychic";
         AttackPowerAcc[14] = "x/100";
         AttackUses[14] = 20;
         AttackDesc[14] = "calm ones mind to raise special attack and defense";
+        AttackTypeValue[14] = 11;
 
         AttackName[15] = "gust";
         AttackType[15] = "air";
         AttackPowerAcc[15] = "40/100";
         AttackUses[15] = 10;
-        AttackDesc[15] = "switches target out with another random party member";
+        AttackDesc[15] = "attack not available";
+        AttackTypeValue[15] = 6;
 
         AttackName[16] = "grave energy";
         AttackType[16] = "dark";
         AttackPowerAcc[16] = "80/100";
         AttackUses[16] = 5;
         AttackDesc[16] = "make a pact with the dark gods to deal a powerful blast of energy at the cost of users own defense";
+        AttackTypeValue[16] = 10;
 
         AttackName[17] = "swift strike";
         AttackType[17] = "normal";
@@ -145,90 +153,105 @@ public class AttackDatabase : MonoBehaviour {
         AttackUses[17] = 15;
         AttackDesc[17] = "with nimble percision the user hits the target, this attack always goes first";
         AttackPriority[17] = true;
+        AttackTypeValue[17] = 1;
 
         AttackName[18] = "scoff";
         AttackType[18] = "normal";
         AttackPowerAcc[18] = "x/100";
         AttackUses[18] = 15;
         AttackDesc[18] = "the user mocks the opponent lowering their defense";
+        AttackTypeValue[18] = 1;
 
         AttackName[19] = "only punch";
         AttackType[19] = "normal";
         AttackPowerAcc[19] = "120/100";
         AttackUses[19] = 5;
         AttackDesc[19] = "focus all of the users fighting power into a single punch, takes one turn to charge";
+        AttackTypeValue[19] = 1;
 
         AttackName[20] = "hone body";
         AttackType[20] = "normal";
         AttackPowerAcc[20] = "x/100";
         AttackUses[20] = 10;
         AttackDesc[20] = "the user looks deep within to raise there attack";
+        AttackTypeValue[20] = 1;
 
         AttackName[21] = "psychic torment";
         AttackType[21] = "psychic";
         AttackPowerAcc[21] = "x/100";
         AttackUses[21] = 10;
         AttackDesc[21] = "the user torments the mind of the target lowering their special defense";
+        AttackTypeValue[21] = 11;
 
         AttackName[22] = "psychic shock";
         AttackType[22] = "psychic";
         AttackPowerAcc[22] = "40/100";
         AttackUses[22] = 15;
         AttackDesc[22] = "the users invades the mind of the target invoking real life pain";
+        AttackTypeValue[22] = 11;
 
         AttackName[23] = "beam cannon";
         AttackType[23] = "archane";
         AttackPowerAcc[23] = "90/85";
         AttackUses[23] = 5;
         AttackDesc[23] = "user shoots a powerful beam, a technic known throughout the ages";
+        AttackTypeValue[23] = 12;
 
         AttackName[24] = "psychic defense";
         AttackType[24] = "psychic";
         AttackPowerAcc[24] = "x/100";
         AttackUses[24] = 5;
         AttackDesc[24] = "create a wall of psychic energy raising the users special defense";
+        AttackTypeValue[24] = 11;
 
         AttackName[25] = "ice shard";
         AttackType[25] = "ice";
         AttackPowerAcc[25] = "40/100";
         AttackUses[25] = 15;
         AttackDesc[25] = "launch a shard of ice at the target with a 5% chance to freeze them";
+        AttackTypeValue[25] = 7;
 
         AttackName[26] = "healing tag";
         AttackType[26] = "light";
         AttackPowerAcc[26] = "x/100";
         AttackUses[26] = 5;
-        AttackDesc[26] = "call upon a teamate to replace the user in battle healing them";
+        AttackDesc[26] = "attack not available";
+        AttackTypeValue[26] = 9;
 
         AttackName[27] = "holy fire";
         AttackType[27] = "fire";
         AttackPowerAcc[27] = "60/100";
         AttackUses[27] = 15;
         AttackDesc[27] = "the user calls upon the fire of the gods, unfortunatly the divine touch does have a chance to cure status effects";
+        AttackTypeValue[27] = 2;
 
         AttackName[28] = "blessing";
         AttackType[28] = "light";
         AttackPowerAcc[28] = "x/100";
         AttackUses[28] = 10;
         AttackDesc[28] = "the user blesses the party, raising everybodys special defense for 5 turns";
+        AttackTypeValue[28] = 9;
 
         AttackName[29] = "triple strike";
         AttackType[29] = "normal";
         AttackPowerAcc[29] = "15/100";
         AttackUses[29] = 20;
         AttackDesc[29] = "the user unleashes a nimble strike allowing for 3 hits before the target can use an attack";
+        AttackTypeValue[29] = 1;
 
         AttackName[30] = "darkness";
         AttackType[30] = "dark";
         AttackPowerAcc[30] = "x/100";
         AttackUses[30] = 10;
-        AttackDesc[30] = "shrouds the battlefield in darkness lowering the accuracy of all non-dark types for 5 turns";
+        AttackDesc[30] = "reduce the accuracy of the target";
+        AttackTypeValue[30] = 10;
 
         AttackName[31] = "sleep powder";
         AttackType[31] = "nature";
         AttackPowerAcc[31] = "x/85";
         AttackUses[31] = 10;
         AttackDesc[31] = "a special poison that puts the target to sleep";
+        AttackTypeValue[231] = 4;
 
         AttackName[32] = "cheep shot";
         AttackType[32] = "dark";
@@ -236,6 +259,7 @@ public class AttackDatabase : MonoBehaviour {
         AttackUses[32] = 15;
         AttackDesc[32] = "the user sucker punches the target, always going first (even if the target is swutching out)";
         AttackSuperPriority[32] = true;
+        AttackTypeValue[32] = 10;
 
 
 
